1ac2d8928ad4f9c7468af8a8550857667ceb753a
Ringo De Smet <ringo@de-smet.name>
2022-11-19 18:48:25 +0100

    Absolute minimal Antora UI
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/site.js'
+    },
+    plugins: [
+        new FileManagerPlugin({
+            events: {
+                onEnd: [
+                    {
+                        copy: [
+                            {
+                                source: 'src/**/*.hbs',
+                                destination: 'build/'
+                            },
+                            {
+                                source: 'src/helpers/*.js',
+                                destination: 'build/helpers'
+                            }
+                        ]
+                    },
+                    {
+                        archive: [
+                            {
+                                source: './build',
+                                destination: 'build/ui-bundle.zip',
+                                options: { globOptions: { nomount: true } },
+                            },
+                        ]
+                    }
+                ]
+            }
+        })
+    ]
+}
